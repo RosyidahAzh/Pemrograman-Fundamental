e923b94f20e19c4d4f974fdd10b355fd8f50df72
RosyidahAzh <151486208+RosyidahAzh@users.noreply.github.com>
2023-11-22 17:57:36 +0700

    Add files via upload

---
+++ Variabels.java
@@ -0,0 +1,9 @@
+package RosyidAzh;
+
+public class Variabels{
+        public static void main(String[] args) {
+            String alphabet = "abcdefghijklmnopqrstuvwxyz";
+            System.out.println(alphabet);
+
+        }
+    }
